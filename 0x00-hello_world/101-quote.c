11f3c70ce82e0a3c0a932dc1173f9d8bce77aaf8
root@DESKTOP-JPVPUH0 <kudizmahoops@gmail.com>
2023-10-10 04:28:39 +0200

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <unlstd.h>
+#include <unistd.h>
+
 /**
- * main - Program printing a line to the standard error
- * Return: 1 (Success)
- */
+* main - Program printing a line to the standard error
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	return (1);
 }
