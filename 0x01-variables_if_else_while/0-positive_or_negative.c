0a012d05051c477dab056b3c66c4e8d5b5d26e30
root@DESKTOP-JPVPUH0 <kudizmahoops@gmail.com>
2023-10-10 03:05:41 +0200

    0-positive_or_negative.c updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,27 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
- * main - The program prints signum of a number
- * Return: 0 (Success)
+ * main - Program that prints the signum of a number
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
-		printf(%d is positive\n", n);
-	}
-	else if (n<0)
-	{
-		printf(%d is negative\n", n);
-	}
-	else (n==0)
-	{
-		printf(%d is zero\n", n);
-	}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+return (0);
 }
